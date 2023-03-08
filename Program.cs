966e6e95ec4bff9a309954a384a27d6ec418bc64
asif0927 <6zd6jws@code.edu.az>
2023-03-08 18:29:41 +0400

    String elemnts end to first

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string[] countries = { "Azerbaijan","Belguim","Georgia","Turkey","Russia","Germany","Greece" };
+for (int i = countries.Length-1; i >= 0; i--)
+{
+    Console.WriteLine(countries[i]);
+}
